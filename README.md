31df7f156b7b40de53a4f9df337c81eb87ae022f
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2021-02-08 03:14:53 +0200

    init-setup bash script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ubuntu-server-setup
 Ubuntu (Linux) Server Setup Bash Script
+
+## Just Run These Commands
+
+```bash
+curl -o init-setup.sh https://raw.githubusercontent.com/AhmedAlaa23/ubuntu-server-setup/main/init-setup.sh
+
+bash init-setup.sh
+```
---
+++ init-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+echo "__ Ubuntu Setup Bash Script"
+
+echo "__ Update && Upgrade && Clean"
+
+sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt clean
+
+#=======================================
+
+echo "__ Adding New User"
+read -p "Enter The New user Name: " USER_NAME
+adduser $USER_NAME
+
+echo "__ Granting The New User Administrative Privileges"
+usermod -aG sudo $USER_NAME
+echo "__ The New User has Granted Administrative Privileges"
+
+echo "__ $USER_NAME Info"
+id $USER_NAME
+
+echo "__ Copying The SSH Key to the New User"
+rsync --archive --chown=$USER_NAME:$USER_NAME ~/.ssh /home/$USER_NAME
+echo "__ The SSH Key has been Copied to the New User"
+
+#=================================================
+
+echo "__ Setting up Firewall (uwf)"
+ufw allow OpenSSH
+ufw allow http
+ufw allow https
+yes y | ufw enable
+ufw status
+echo "__ Allowed OpenSSH, http, https and enabled uwf"
+
+#=================================================
+
+echo "__ Disabling Root Login (PermitRootLogin no) in /etc/ssh/sshd_config"
+sed -i '/^PermitRootLogin[ \t]\+\w\+$/{ s//PermitRootLogin no/g; }' /etc/ssh/sshd_config
+echo "__ Root Login Disabled"
+
+echo "__ Reloading sshd"
+sudo systemctl reload sshd
+
+echo "__ Deleting The Script File"
+rm -- "$0"
+
+echo "__ Switching to $USER_NAME User"
+cd ./../home/$USER_NAME
+su $USER_NAME
